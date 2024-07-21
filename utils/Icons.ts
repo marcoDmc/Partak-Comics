4e55e3d73729e7966057cb36d61d3c5f8204b985
marcodmc <marcodamasceno07@gmail.com>
2024-07-21 09:17:36 -0300

    chore: created separate files for exporting items for better organization

---
+++ utils/Icons.ts
@@ -0,0 +1,5 @@
+import { GoArrowUpRight } from "react-icons/go";
+
+export const Icons = {
+    arrowUpRight: GoArrowUpRight
+}
---
+++ utils/files.ts
@@ -0,0 +1,56 @@
+import money from "@/public/money.png"
+import eightball from "@/public/8-ball.png"
+import bomb from "@/public/bomb.png"
+import cartridge from "@/public/cartridge.png"
+import astroboy from "@/public/astroboy.png"
+import cigarette from "@/public/cigarette.png"
+import coffin from "@/public/coffin.png"
+import control from "@/public/control.png"
+import disk from "@/public/disk.png"
+import globe from "@/public/globe.png"
+import hand from "@/public/hand.png"
+import hotdog from "@/public/hot-dog.png"
+import logomain from "@/public/logo-main.png"
+import lolipop from "@/public/lolipop.png"
+import meat from "@/public/meat.png"
+import mirror from "@/public/mirror.png"
+import paper from "@/public/paper.png"
+import partaklogo from "@/public/partak-logo.png"
+import pin from "@/public/pin.png"
+import pizza from "@/public/pizza.png"
+import trapbear from "@/public/trap-bear.png"
+import spray from "@/public/spray.png"
+import skate from "@/public/skate.png"
+import sandal from "@/public/sandal.png"
+import robot from "@/public/robot.png"
+import poison from "@/public/poison.png"
+
+
+export const Files = {
+    money,
+    eightball,
+    bomb,
+    cartridge,
+    astroboy,
+    cigarette,
+    coffin,
+    control,
+    disk,
+    globe,
+    hand,
+    hotdog,
+    logomain,
+    lolipop,
+    meat,
+    mirror,
+    paper,
+    partaklogo,
+    pin,
+    pizza,
+    trapbear,
+    spray,
+    skate,
+    sandal,
+    robot,
+    poison
+}
